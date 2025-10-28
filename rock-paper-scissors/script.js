9a277d9d7c48c03772d042104ea2e04d5bde037b
mahakagarwal7 <mahakagrwl07@gmail.com>
2025-10-28 07:18:52 +0530

    Added my project: Rock-Paper-Scissors Game

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,38 @@
+function play(playerChoice) {
+  var choices = ['rock', 'paper', 'scissors'];
+   var computerChoice = choices[Math.floor(Math.random() * 3)];
+  var result = '';
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie! You both chose " + playerChoice + ".";
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors')||
+    (playerChoice === 'paper' && computerChoice === 'rock')||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You win! " + playerChoice + " beats " + computerChoice + ".";
+  } else {
+    result = "You lose! " + computerChoice + " beats " + playerChoice + ".";
+  }
+
+  document.getElementById('result').textContent = result;
+}
+function play(playerChoice) {
+  var choices = ['rock', 'paper', 'scissors'];
+  var computerChoice = choices[Math.floor(Math.random() * 3)];
+  var result = '';
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie! You both chose " + playerChoice + ".";
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You win! " + playerChoice + " beats " + computerChoice + ".";
+  } else {
+    result = "You lose! " + computerChoice + " beats " + playerChoice + ".";
+  }
+
+  document.getElementById('result').textContent = result;
+}
